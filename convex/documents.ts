c7a4cdd7c7d1edc789ab210337d422d7c56c564b
Mehroz Ali <dev.mehrozali@gmail.com>
2023-10-29 18:25:59 +0500

    Archive function

--- convex/documents.ts
+++ convex/documents.ts
@@ -52,6 +52,49 @@ export const getSidebar = query({
   },
 });
 
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+    const existingDoc = await ctx.db.get(args.id);
+
+    if (!existingDoc) {
+      throw new Error("Not found!");
+    }
+
+    if (existingDoc.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, { isArchived: true });
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true,
+    });
+    recursiveArchive(args.id);
+
+    return document;
+  },
+});
+
 export const get = query({
   handler: async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
